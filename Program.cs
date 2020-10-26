0be946fc8e185ca97afa8cd7f61e777f2ea9d267
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-27 04:17:11 +0530

    [Prajval]Solotion started

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMaximum_Day15
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the practice problem of day15");
+        }
+    }
+}
