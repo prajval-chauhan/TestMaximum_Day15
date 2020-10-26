fba1b8cbeca5b4e7e6acdaae680786ffe4378e91
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-27 04:30:33 +0530

    Method to find max of three int is added

---
+++ CheckMaximum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TestMaximum_Day15
+{
+    class CheckMaximum
+    {
+        public int CheckMaximumInt (int firstNum, int secondNum, int thirdNum)
+        {
+            Console.Write("Maximum of the three number is : ");
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            else if (secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else if (thirdNum.CompareTo(secondNum) > 0)
+                return thirdNum;
+            else
+            {
+                Console.WriteLine("All the integers are equal");
+                return 0;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,7 +6,9 @@ namespace TestMaximum_Day15
     {
         static void Main(string[] args)
         {
+            CheckMaximum call = new CheckMaximum();
             Console.WriteLine("Welcome to the practice problem of day15");
+            Console.Write(call.CheckMaximumInt(4, 4, 4)); 
         }
     }
 }
