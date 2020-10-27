df9bc2073eb547ece742e77ad53e796aa692c2d6
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-27 06:33:06 +0530

    [Prajval]Print functionality added

--- CheckMaximum.cs
+++ CheckMaximum.cs
@@ -7,6 +7,72 @@ namespace TestMaximum_Day15
     class CheckMaximum
     {
         /// <summary>
+        /// Checks the maximum integer of the given three integers
+        /// </summary>
+        /// <param name="firstNum">The first number.</param>
+        /// <param name="secondNum">The second number.</param>
+        /// <param name="thirdNum">The third number.</param>
+        /// <returns></returns>
+        public int CheckMaximumInt(int firstNum, int secondNum, int thirdNum)
+        {
+            Console.Write("Maximum of the three number is : ");
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            else if (secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else if (thirdNum.CompareTo(secondNum) > 0)
+                return thirdNum;
+            else
+            {
+                Console.WriteLine("All the integers are equal");
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Checks the maximum double of the given three double
+        /// </summary>
+        /// <param name="firstNum">The first number.</param>
+        /// <param name="secondNum">The second number.</param>
+        /// <param name="thirdNum">The third number.</param>
+        /// <returns></returns>
+        public double CheckMaximumDouble(double firstNum, double secondNum, double thirdNum)
+        {
+            Console.Write("Maximum of the three number is : ");
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            else if (secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else if (thirdNum.CompareTo(secondNum) > 0)
+                return thirdNum;
+            else
+            {
+                Console.WriteLine("All the integers are equal");
+                return 0;
+            }
+        }
+        /// <summary>
+        /// Checks the maximum string of the given three strings of same length
+        /// </summary>
+        /// <param name="firstNum">The first number.</param>
+        /// <param name="secondNum">The second number.</param>
+        /// <param name="thirdNum">The third number.</param>
+        /// <returns></returns>
+        public string CheckMaximumString(string firstNum, string secondNum, string thirdNum)
+        {
+            Console.Write("Maximum of the three number is : ");
+            if (firstNum.CompareTo(secondNum) > 0 && firstNum.CompareTo(thirdNum) > 0)
+                return firstNum;
+            else if (secondNum.CompareTo(thirdNum) > 0)
+                return secondNum;
+            else if (thirdNum.CompareTo(secondNum) > 0)
+                return thirdNum;
+            else
+            {
+                Console.WriteLine("All the integers are equal");
+                return null;
+            }
+        }
+        /// <summary>
         /// Checks the maximum method generic
         /// </summary>
         /// <typeparam name="T"></typeparam>
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics.CodeAnalysis;
 
 namespace TestMaximum_Day15
 {
@@ -8,9 +9,15 @@ namespace TestMaximum_Day15
         {
             CheckMaximum call = new CheckMaximum();
             Console.WriteLine("Welcome to the practice problem of day15");
+            //Usin non generic methods to compare
+            Console.WriteLine(call.CheckMaximumInt(1,2,3));
+            Console.WriteLine(call.CheckMaximumDouble(1.1,1.2,1.111));
+            Console.WriteLine(call.CheckMaximumString("11","22","33"));
+            //Using generic method to compare
             Console.WriteLine(call.CheckMaximumGeneric<int>(1, 2, 3));
             Console.WriteLine(call.CheckMaximumGeneric<string>("22","22","22"));
             Console.WriteLine(call.CheckMaximumGeneric<double>(1.1,1.11,1.111));
+            //using generic class to compare multiple values
             int[] array = { 1, 2, 3, 4, 5, 6, 7, 8 };
             CheckingMaxGenerics<int> checkingMaxGenerics = new CheckingMaxGenerics<int>(array);
             Console.WriteLine("Maximum value in the array is : " +checkingMaxGenerics.MaximumValue(array));
